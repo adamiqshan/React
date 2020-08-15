b4b1b27b7c6b14424d56795c6c0fb6512e092da1
adam iqshan <omai.mohmed9@gmail.com>
2020-08-15 23:35:32 +0530

    Ex 01 end

--- reacter/src/index.js
+++ reacter/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Congrats Adam</h1>,
+  <div>
+    <h1>Congrats Adam</h1>
+    <ul>
+      <li>Kafka</li>
+      <li>Ikigai</li>
+      <li>Mongoose</li>
+    </ul>
+  </div>
+  ,
   document.getElementById('root')
 );
