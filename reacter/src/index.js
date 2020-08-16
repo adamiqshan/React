7acfb4e2135253bcddb20c3cf5d6dfef123b58cd
adam iqshan <omai.mohmed9@gmail.com>
2020-08-17 03:22:47 +0530

    added styles

--- reacter/src/index.js
+++ reacter/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// var randomImg = 'https://picsum.photos/200'
+
 ReactDOM.render(
   <div>
-    <h1>Congrats Adam</h1>
+    <h1 className='heading' contentEditable='true' spellCheck='false'>Recent Books I read</h1>
     <ul>
-      <li>Kafka</li>
+      <li>Kafka on the shore</li>
+      <img src="images/kafka.jpg" alt=""/>
       <li>Ikigai</li>
-      <li>Mongoose</li>
+      <img src="images/ikigai.jpg" alt=""/>
+      <li>Monk who sold his Ferrari</li>
+      <img src="images/monk.jpg" alt=""/>
     </ul>
+    {/* <img src={randomImg} alt=""/> */}
   </div>
   ,
   document.getElementById('root')
