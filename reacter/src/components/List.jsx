d0d033a456d2ad4235b617c4834fc95d8ddfd9bf
adam iqshan <omai.mohmed9@gmail.com>
2020-08-20 18:29:48 +0530

    added components

---
+++ reacter/src/components/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Kafka on the shore</li>
+      <img src="images/kafka.jpg" alt=""/>
+      <li>Ikigai</li>
+      <img src="images/ikigai.jpg" alt=""/>
+      <li>Monk who sold his Ferrari</li>
+      <img src="images/monk.jpg" alt=""/>
+    </ul>
+  );
+}
+
+export default List;
--- reacter/src/index.js
+++ reacter/src/index.js
@@ -1,21 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-// var randomImg = 'https://picsum.photos/200'
-
-ReactDOM.render(
-  <div>
-    <h1 className='heading' contentEditable='true' spellCheck='false'>Recent Books I read</h1>
-    <ul>
-      <li>Kafka on the shore</li>
-      <img src="images/kafka.jpg" alt=""/>
-      <li>Ikigai</li>
-      <img src="images/ikigai.jpg" alt=""/>
-      <li>Monk who sold his Ferrari</li>
-      <img src="images/monk.jpg" alt=""/>
-    </ul>
-    {/* <img src={randomImg} alt=""/> */}
-  </div>
-  ,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
