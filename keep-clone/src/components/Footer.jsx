4eb1b3a76de8ed06d95a8499e48224d60c380d37
adam iqshan <omai.mohmed9@gmail.com>
2020-08-26 18:46:23 +0530

    final version

--- keep-clone/src/components/Footer.jsx
+++ keep-clone/src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-function Footer(){
-
-    let year = new Date().getFullYear()
-    return (
-        <footer>
-            <p>Copyrights ⓒ {year}</p>
-        </footer>
-    );
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
